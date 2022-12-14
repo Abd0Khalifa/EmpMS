8ea957a36c4682808e4bae0aaf29944b3dd6d929
Abdulrahman Khalifa <103148487+Abd0Khalifa@users.noreply.github.com>
2022-12-14 19:50:28 +0200

    Abdulrahman Khalifa (Create DeleteBtn)

--- EmpMS/Departments.cs
+++ EmpMS/Departments.cs
@@ -102,7 +102,27 @@ namespace EmpMS
 
         private void DeleteBtn_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if (DepNameTb.Text == "")
+                {
+                    MessageBox.Show("Missing Data!!!");
+                }
+                else
+                {
+                    string Dep = DepNameTb.Text;
+                    string Query = "Delete from DepartmentTbl where Depid = {0}";
+                    Query = string.Format(Query, Key);
+                    Con.SetData(Query);
+                    ShowDepartments();
+                    MessageBox.Show("Department Deleted");
+                    DepNameTb.Text = "";
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
         }
     }
 }
